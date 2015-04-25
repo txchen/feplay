c59267be42e09f68fdc578430f214f9d385991fc
Tianxiang Chen <txchendev@gmail.com>
2015-04-25 14:06:29 -0700

    fix bug when there is no local storage

--- riot_webpack/src/store/blogstore.js
+++ riot_webpack/src/store/blogstore.js
@@ -4,11 +4,29 @@ function BlogStore() {
   if (!(this instanceof BlogStore)) return new BlogStore()
   riot.observable(this)
 
+  let saveToStorage = () => {
+    window.localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(this._posts))
+  }
+
+  let initData = () => {
+    let defaultPosts = [
+      {postId: 1, title: 'Best xbox games', content: 'Halo, GOW', category: 'collection', likes: 10},
+      {postId: 2, title: 'Best ps games', content: 'Uncharted, The Last of US', category: 'collection', likes: 20},
+      {postId: 3, title: 'Best wii games', content: 'Zelda, Mario', category: 'collection', likes: 16},
+      {postId: 4, title: 'Review of Halo', content: 'yes, cortana', category: 'review', likes: 11},
+      {postId: 5, title: 'Review of Titanfall', content: 'where is the local game?', category: 'review', likes: 7},
+      {postId: 6, title: 'Review of portal', content: 'I don\'t blame you', category: 'review', likes: 40},
+    ]
+    this._posts = defaultPosts
+    saveToStorage()
+  }
+
   let json = window.localStorage.getItem(LOCALSTORAGE_KEY)
   if (!json) {
     initData()
+  } else {
+    this._posts = (json && JSON.parse(json)) || []
   }
-  this._posts = (json && JSON.parse(json)) || []
 
   this.on(riot.VE.LOAD_POSTS, () => {
     this.trigger(riot.SE.POSTS_CHANGED, this._posts)
@@ -31,23 +49,6 @@ function BlogStore() {
 
   this.getPostById = id => {
     return this._posts.filter(p => p.postId == id)[0]
-  }
-
-  let initData = () => {
-    let defaultPosts = [
-      {postId: 1, title: 'Best xbox games', content: 'Halo, GOW', category: 'collection', likes: 10},
-      {postId: 2, title: 'Best ps games', content: 'Uncharted, The Last of US', category: 'collection', likes: 20},
-      {postId: 3, title: 'Best wii games', content: 'Zelda, Mario', category: 'collection', likes: 16},
-      {postId: 4, title: 'Review of Halo', content: 'yes, cortana', category: 'review', likes: 11},
-      {postId: 5, title: 'Review of Titanfall', content: 'where is the local game?', category: 'review', likes: 7},
-      {postId: 6, title: 'Review of portal', content: 'I don\'t blame you', category: 'review', likes: 40},
-    ]
-    this._posts = defaultPosts
-    saveToStorage()
-  }
-
-  let saveToStorage = () => {
-    window.localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(this._posts))
   }
 }
 
