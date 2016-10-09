d79bdf17ac0f70275aca12fe84b9dfad421e92ab
Tianxiang Chen <txchendev@gmail.com>
2016-10-08 17:41:15 -0700

    try to use popover

--- elementtest/src/components/Hello.vue
+++ elementtest/src/components/Hello.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <el-popover
+      ref="popover2"
+      placement="top-start"
+      title="标题"
+      width="200"
+      trigger="hover"
+      content="这是一段容,这是一段容,这是一段容,这是一段容。">
+    </el-popover>
+    <el-table :data="rows" stripe border>
+      <el-table-column property="a" label="a" sortable></el-table-column>
+      <el-table-column inline-template label="b">
+        <el-button v-popover:popover2>{{ row.b }}</el-button>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -8,7 +22,12 @@
 export default {
   data () {
     return {
-      msg: 'Hello Vue!'
+      msg: 'Hello Vue!',
+      rows: [
+        { a: 1, b: 2 },
+        { a: 3, b: 4 },
+        { a: 5, b: 6 }
+      ]
     }
   }
 }
--- elementtest/src/main.js
+++ elementtest/src/main.js
@@ -2,6 +2,19 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Popover from 'element-ui/lib/popover'
+import 'element-ui/lib/theme-default/popover.css'
+import Button from 'element-ui/lib/button'
+import 'element-ui/lib/theme-default/button.css'
+import Table from 'element-ui/lib/table'
+import 'element-ui/lib/theme-default/table.css'
+import TableColumn from 'element-ui/lib/table-column'
+import 'element-ui/lib/theme-default/table-column.css'
+
+Vue.component(Button.name, Button)
+Vue.component(Popover.name, Popover)
+Vue.component(Table.name, Table)
+Vue.component(TableColumn.name, TableColumn)
 
 /* eslint-disable no-new */
 new Vue({
